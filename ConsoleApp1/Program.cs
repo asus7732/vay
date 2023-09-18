210b005761e260186a4b4b561f3894e704b5193f
asus7732 <nurulloev.2006@gmail.com>
2023-09-19 01:03:01 +0300

    Я старался

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double one, two;
+            string birth;
+            Console.WriteLine("Введите первое число");
+            one = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите второе число");
+            two = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Выберете операцию: '+' '-' '*' '/'");
+            birth = (Console.ReadLine());
+            
+            if (birth == "+")
+            {
+                Console.WriteLine(one + two);
+            }
+            else if (birth == "-")   
+            {
+                Console.WriteLine(one - two);   
+            }
+            else if ( birth == "*")
+            {
+                Console.WriteLine(one * two);
+            }
+            else if( birth == "/")
+            {
+                if (one == 0)
+                    Console.WriteLine(0);
+                else
+                    Console.WriteLine(one / two);
+            }
+            else
+            {
+                Console.WriteLine("Ошибка! Неизвестное действие!");
+            }
+            Console.ReadLine(); 
+
+        }
+
+    }
+   
+    
+   
+}
+
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true) 
+            {
+
+                Console.Clear();
+                double one, two;
+                string birth;
+                try 
+                {
+                    Console.WriteLine("Введите первое число");
+                    one = double.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Введите второе число");
+                    two = double.Parse(Console.ReadLine());
+                }
+                catch (Exception)  
+                {
+                    Console.Write("Не удалось переобразовать строку в число!");
+                    Console.ReadLine();
+                    continue;
+                }
+
+                Console.WriteLine("Выберете операцию: '+' '-' '*' '/'");
+                birth = (Console.ReadLine());
+
+                Console.Clear();
+                switch (birth)
+                {
+                    case "+":
+                        Console.WriteLine(one + two);
+
+                        break;
+
+                    case "-":
+
+                        Console.WriteLine(one - two);
+
+                        break;
+
+                    case "*":
+                        Console.WriteLine(one * two);
+
+
+
+                        break;
+
+                    case "/":
+                        Console.WriteLine(one / two);
+
+                        break;
+
+                    default:
+                        Console.WriteLine("Ошибка! Неизвестное действие!");
+                        break;
+
+
+
+                }
+
+                Console.ReadLine();
+            }
+                }
+            }
+            
+
+           
+
+
+        }
